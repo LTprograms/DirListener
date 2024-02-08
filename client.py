b46182635330b14b2ee747c6958af60de3999a57
Luis LT <105554066+LTprograms@users.noreply.github.com>
2024-02-08 01:53:26 -0500

    .

--- client.py
+++ client.py
@@ -4,23 +4,15 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class MyHandler(FileSystemEventHandler):
-    __msg = ""
-
     def on_created(self, event):
         if event.is_directory:
             msg = f"Se ha creado un directorio: {event.src_path}"
         else:
             msg = f"Se ha creado un archivo: {event.src_path}"
-        
-        self.__msg = msg
-        print(self.__msg)
 
-    @property
-    def message(self):
-        return self.__msg
-    
-    def set_msg(self, msg):
-        self.__msg = msg
+        # Enviar el mensaje al servidor cuando se crea un archivo
+        client.sendall(msg.encode('utf-8'))
+        print(f"Enviado: {msg}")
 
 # Configuración del cliente
 host = '192.168.1.19'
@@ -29,7 +21,7 @@ port = 12345
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((host, port))
 
-    # Crear el observador y el manejador de eventos fuera del bucle
+    # Crear el observador y el manejador de eventos
     event_handler = MyHandler()
     observer = Observer()
 
@@ -47,17 +39,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             observer.schedule(event_handler, path=directory, recursive=True)
             observer.start()
 
-            # Esperar hasta que se reciba un mensaje del manejador de eventos
-            observer.join()
-
-            # Enviar el mensaje al servidor
-            client.sendall(event_handler.message.encode('utf-8'))
-            print(f"Enviado: {event_handler.message}")
-
-            # Limpiar el mensaje después de enviarlo
-            event_handler.set_msg("")
+            try:
+                while True:
+                    pass
+            except KeyboardInterrupt:
+                observer.stop()
 
     except KeyboardInterrupt:
-        observer.stop()
+        pass
 
 print("Cliente desconectado.")
